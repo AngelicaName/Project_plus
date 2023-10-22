506f29a96ce35f772dc996b9e84fbd1a65ca9f44
AngelicaName <140308936+AngelicaName@users.noreply.github.com>
2023-10-23 02:15:26 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,165 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+function minutesFormat() {
+  if (minutes < 10) {
+    return `0${minutes}`;
+  } else {
+    return `${minutes}`;
+  }
+}
+
+let todayDate = document.querySelector("#now");
+todayDate.innerHTML = `${day} ${hours}:${minutesFormat()} `;
+
+//////////////////////////////////////////////////////////////
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  return days[day];
+}
+
+function forecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="75"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(forecast);
+}
+
+function weather(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+}
+
+function searchCity(city) {
+  let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weather);
+}
+
+function  typeCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input"); 
+  searchCity(cityInput.value);
+    }   
+
+function displayFahrenheiTemperature(event) { 
+  let temperatureElement = document.querySelector("#temperature"); 
+  let fahrenheiTemperature =  celsiusTemperature * 9 / 5 + 32;
+  console.log(fahrenheiTemperature);
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+  }
+
+  function displayCelsiusTemperature(event) {
+  let temperatureElement = document.querySelector("#temperature");  
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  console.log(celsiusTemperature);
+  }
+
+let  celsiusTemperature = null;
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", typeCity);   
+
+let fahrenheiLink = document.querySelector("#fahrenhei-link");
+fahrenheiLink.addEventListener("click", displayFahrenheiTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+searchCity("Kyiv");
